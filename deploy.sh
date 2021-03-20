d79f0b9eabe7c3864bbf1d336b4e84066e4da313
nurlimandiara <nurlimandiara@gmail.com>
2021-03-20 17:18:48 +0700

    update script

--- deploy.sh
+++ deploy.sh
@@ -6,14 +6,14 @@ IMAGE_TAG=`git rev-parse HEAD`
 REPO_NAME="gyas-wedding-invitation"
 GCP_PROJECT_NAME="data-media-283004"
 
-docker build . -t $REPO_NAME:$IMAGE_TAG
+docker build . -t gcr.io/$GCP_PROJECT_NAME/$REPO_NAME:$IMAGE_TAG
 
-docker tag $REPO_NAME gcr.io/$GCP_PROJECT_NAME/$REPO_NAME:$IMAGE_TAG
+# docker tag $REPO_NAME gcr.io/$GCP_PROJECT_NAME/$REPO_NAME:$IMAGE_TAG
 
 docker push gcr.io/$GCP_PROJECT_NAME/$REPO_NAME:$IMAGE_TAG
 
 gcloud beta run deploy $REPO_NAME --image gcr.io/$GCP_PROJECT_NAME/$REPO_NAME:$IMAGE_TAG \
   --project $GCP_PROJECT_NAME \
   --platform managed \
-  --region asia-northeast1 \
+  --region asia-southeast1 \
   --allow-unauthenticated
